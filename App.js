e66391a44064b0fbdd48a32e7daf4c43f7d57b37
Guilherme Serra <serra.guilherme@gmail.com>
2021-02-05 18:01:11 -0300

    Primeiro Commit.

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView, StatusBar, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#000" barStyle="light-content"/>
+      <ScrollView>
+
+        <Text style={styles.title}>Help Invest</Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('./assets/buttons/retangulo_azul.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('./assets/buttons/retangulo_vermelho.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image style={styles.img}
+            source={require('./assets/buttons/retangulo_verde.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('./assets/buttons/retangulo_roxo.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('./assets/buttons/retangulo_amarelo.png')}
+          />
+        </TouchableOpacity>
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#151515',
   },
+  title: {
+    textAlign: 'left',
+    fontSize: 25,
+    color: '#FFF',
+    backgroundColor: '#000',
+    padding: 15
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 3,
+    padding: 5
+  },
+  textButton: {
+    fontSize: 55,
+    color: '#FFF',
+    position: 'absolute',
+    textAlign: 'left'
+  },
+  img: {
+    width: 378,
+    height: 130,
+    borderRadius: 10
+  }
 });
