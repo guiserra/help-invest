5005d3a2b1898e60e58bb28685cdea64076f9c88
catakevin98 <catakevin98@github.com>
2021-02-11 17:03:53 -0300

    Concluido a parte de Fluxo de Caixa

--- src/pages/Indicadores/DividaBruta/index.js
+++ src/pages/Indicadores/DividaBruta/index.js
@@ -10,18 +10,18 @@ export default function DividaBruta() {
                 <View style={[styles.cardBoard, styles.shadow, { marginTop: 10 }]}>
                     <View style={styles.cardContent}>
                         <Text style={[styles.title, styles.shadow]}>Definição</Text>
-                        <Text style={[styles.content, styles.shadow]}>   A
+                        <Text style={[styles.content, styles.shadow]}>  A
                             <Text style={[styles.contentBold, styles.shadow]}> Dívida Bruta</Text> é um valor composto pela
                             soma de todas as obrigações financeiras da empresa, isto é, todas as dívidas da instituição que
                             estão em aberto para serem devidamente quitadas. Ressaltando que, devem ser levadas em consideração
                             as dívidas de curto e longo prazo.
                         </Text>
-                        <Text style={[styles.content, styles.shadow]}>   Utilizado para verificar a saúde financeira e o grau de
+                        <Text style={[styles.content, styles.shadow]}>  Utilizado para verificar a saúde financeira e o grau de
                         endividamento das empresas, o conceito de <Text style={[styles.contentBold, styles.shadow]}> dívida bruta</Text>,
                         também pode ser relacionado às dívidas que são adquiridas pela empresa para financiar o capital de giro da
                         instituição ou expandir seus negócios e operações.
                         </Text>
-                        <Text style={[styles.content, styles.shadow]}>   Sendo assim, a empresa pode formar um inventário contábil
+                        <Text style={[styles.content, styles.shadow]}>  Sendo assim, a empresa pode formar um inventário contábil
                         do quantitativo de dívidas que devem ser geridas, buscando um planejamento financeiro empresarial, para
                         quitar tanto dívidas já realizadas, como obrigações futuras.
                         </Text>
@@ -31,15 +31,15 @@ export default function DividaBruta() {
                 <View style={[styles.cardBoard, styles.shadow]}>
                     <View style={styles.cardContent}>
                         <Text style={[styles.title, styles.shadow]}>Como interpretar a dívida bruta? </Text>
-                        <Text style={[styles.content, styles.shadow]}>   A dívida bruta pode ser interpretada como um valor utilizado 
-                        para verificar o grau de endividamento de uma empresa. Assim, para conseguir interpretar a dívida bruta será 
+                        <Text style={[styles.content, styles.shadow]}>   A dívida bruta pode ser interpretada como um valor utilizado
+                        para verificar o grau de endividamento de uma empresa. Assim, para conseguir interpretar a dívida bruta será
                         necessário somar todos os compromissos e obrigações da instituição.
                         {"\n"}
                             {"\n"}
-                            Desse modo, será possível ter uma visão ampliada do modelo financeiro utilizado pela empresa que detém o 
-                            endividamento, como também, o analista conseguirá verificar as dívidas de curto, médio e longo prazo, que 
-                            foram adquiridas pela empresa.
-                        {"\n"}
+                            <Text style={[styles.content, styles.shadow]}>   Desse modo, será possível ter uma visão ampliada do modelo financeiro utilizado pela empresa que detém o
+                            endividamento, como também, o analista conseguirá verificar as dívidas de curto, médio e longo prazo, que
+                            foram adquiridas pela empresa. </Text>
+                            {"\n"}
                             {"\n"}
 
                         </Text>
@@ -54,8 +54,9 @@ export default function DividaBruta() {
                         dívidas da empresa durante um curto período de tempo, com as dívidas adquiridas para longo prazo.
                         {"\n"}
                             {"\n"}
-                        Sendo assim, será possível verificar o endividamento da empresa sobre seu patrimônio, através da seguinte fórmula:
-                        {"\n"}
+                            <Text style={[styles.content, styles.shadow]}>    Sendo assim, será possível verificar o endividamento da
+                        empresa sobre seu patrimônio, através da seguinte fórmula: </Text>
+                            {"\n"}
                             {"\n"}
                         DB = Divida Bruta; {"\n"}
                         DE = Divida da Empresa. {"\n"}
@@ -69,16 +70,16 @@ export default function DividaBruta() {
                 <View style={[styles.cardBoard, styles.shadow]}>
                     <View style={styles.cardContent}>
                         <Text style={[styles.title, styles.shadow]}>Limitações da Dívida Bruta</Text>
-                        <Text style={[styles.content, styles.shadow]}>   A dívida bruta possibilita que o analista consiga 
+                        <Text style={[styles.content, styles.shadow]}>   A dívida bruta possibilita que o analista consiga
                         avaliar o grau de endividamento da empresa, verificando o compromisso e as obrigações possuídas pela instituição.
                         {"\n"}
                             {"\n"}
-                            Sendo assim, a dívida bruta está entre os principais valores no mercado financeiro utilizados para 
-                            verificar a saúde financeira da empresa de maneira mais assertiva.
-                        {"\n"}
+                            <Text style={[styles.content, styles.shadow]}>    Sendo assim, a dívida bruta está entre os principais valores no mercado financeiro utilizados para
+                            verificar a saúde financeira da empresa de maneira mais assertiva. </Text>
                             {"\n"}
-                            Porém, a dívida bruta possui limitações, dessa forma, não é recomendado analisar esse valor de forma isolada, 
-                            e sim utilizar diversas ferramentas de análise, de forma simultânea e complementar.
+                            {"\n"}
+                            <Text style={[styles.content, styles.shadow]}>    Porém, a dívida bruta possui limitações, dessa forma, não é recomendado analisar esse valor de forma isolada,
+                            e sim utilizar diversas ferramentas de análise, de forma simultânea e complementar. </Text>
                         </Text>
 
                     </View>
--- src/pages/Indicadores/FluxoCaixa/index.js
+++ src/pages/Indicadores/FluxoCaixa/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, SafeAreaView, StyleSheet, Text, ScrollView, Linking } from 'react-native';
+import { View, SafeAreaView, StyleSheet, Text, Image, ScrollView, Linking } from 'react-native';
 
 export default function FluxoCaixa() {
   return (
@@ -10,19 +10,92 @@ export default function FluxoCaixa() {
           <View style={styles.cardContent}>
             <Text style={[styles.title, styles.shadow]}>Definição</Text>
             <Text style={[styles.content, styles.shadow]}>   O
-                  <Text style={[styles.contentBold, styles.shadow]}> Fluxo de Caixa</Text> é o indicador mais importante de
-                  uma empresa, ele irá mostrar o saldo das entradas (+) e saídas (-) financeiras da empresa. Como diz a
-                  expressão: “O caixa é rei”, pois é ele que irá refletir o dia a dia do negócio, o dinheiro disponível
-                  para sua utilização nas mais diversas aplicações. O caixa mostra, em médio e longo prazo, a saúde
-                  financeira de uma empresa.
+                  <Text style={[styles.contentBold, styles.shadow]}> Fluxo de Caixa</Text> nada mais é do que a soma
+                  de todo o dinheiro que entra e que sai da empresa. É através dele que o empresário consegue visualizar
+                  como está gastando o seu dinheiro e quanto de lucro ele tem tido.
             </Text>
-            <Text style={[styles.content, styles.shadow]}>   O caixa pode quebrar uma empresa, pois se realizarmos as
-            vendas com lucro, porém os prazos de recebimentos forem muito maiores que os prazos de pagamento, e essa
-            empresa não estiver preparada para essa operação, isso pode quebra-la.
+            <Text style={[styles.content, styles.shadow]}>   Falando assim já fica fácil de entender a importância de se
+            manter um bom controle de fluxo de caixa. Isso porque, somente conhecendo quais são as despesas e quais as
+            receitas é que é possível melhorar o gerenciamento financeiro de qualquer negócio, seja ele de pequeno,
+            médio ou grande porte.
             </Text>
-            <Text style={[styles.contentBold, styles.shadow]}>   Vejamos um exemplo à seguir:
+            <Text style={[styles.content, styles.shadow]}>   De forma simples, o fluxo de caixa de uma empresa é
+            semelhante a um orçamento doméstico. Se a pessoa não sabe quanto recebe todos os meses, e nem quais os gastos
+            que ela tem, as chances de fechar no vermelho ou de faltar dinheiro para pagar alguma conta são grandes.
             </Text>
-            
+          </View>
+        </View>
+
+        <View style={[styles.cardBoard, styles.shadow, { marginTop: 10 }]}>
+          <View style={styles.cardContent}>
+            <Text style={[styles.title, styles.shadow]}>Como calcular o Fluxo de Caixa?</Text>
+            <Text style={[styles.content, styles.shadow]}>   O primeiro passo para calcular o fluxo de caixa é ter
+            absolutamente todas as receitas e despesas anotadas. Mesmo aquelas pequenas, que parecem ser sem importância.
+            </Text>
+            <Text style={[styles.content, styles.shadow]}>   Em seguida, será preciso somar todas as receitas, que geralmente
+            são provenientes das operações que a empresa realiza (vendas de produtos e serviços) e dos investimentos que a
+            empresa recebe.
+            </Text>
+            <Text style={[styles.content, styles.shadow]}>   O próximo passo será somar todos os gastos que a empresa teve no
+            mesmo período. Esses valores são provenientes dos gastos operacionais (aluguel, compra de matéria-prima, salários
+            de funcionários, por exemplo), pagamentos de impostos, dividendos, compra de equipamentos, dentre outros.
+            </Text>
+            <Text style={[styles.content, styles.shadow]}>   Para o cálculo, é fundamental que seja utilizado o mesmo período
+            para a soma das receitas e dos gastos. Em seguida, basta subtrair do valor obtido de receita o valor obtido das
+            despesas:
+            </Text>
+            <Text style={[styles.title, styles.shadow]}>Cálculo operacional = Receitas - Despesas</Text>
+          </View>
+        </View>
+        
+
+        <View style={[styles.cardBoard, styles.shadow, { marginTop: 10 }]}>
+          <View style={styles.cardContent}>
+            <Text style={[styles.title, styles.shadow]}>Como controlar o fluxo de caixa</Text>
+            <Text style={[styles.content, styles.shadow]}>   Empresas que mantém um bom controle de fluxo de caixa podem
+            realizar investimentos tranquilamente e ainda conseguir se planejar para o futuro, como buscar linhas de
+            créditos para a sua empresa. Veja a seguir algumas dicas para manter o controle do seu fluxo de caixa:
+            </Text>
+
+            <Text style={[styles.content, styles.shadow]}>
+              <Text style={[styles.subtitle, styles.shadow]}>   Saldo inicial </Text>
+              {"\n"}{"\n"}
+              <Text style={[styles.content, styles.shadow]}>    A primeira coisa que deve ser feita é saber exatamente quanto
+              a empresa tem disponível em caixa. Para isso, basta somar o saldo de todas as contas bancárias que a empresa possui.
+             </Text>
+              {"\n"}{"\n"}
+
+
+              <Text style={[styles.subtitle, styles.shadow]}>   Anote as entradas e saídas de dinheiro </Text>
+              {"\n"}{"\n"}
+              <Text style={[styles.content, styles.shadow]}>   Em seguida, será necessário anotar todas as receitas e os gastos
+              que a empresa teve. Esse controle pode ser feito em uma planilha diária, semanal ou até mesmo mensal. O mais importante
+              é não esquecer de anotar os gastos.
+                {"\n"}{"\n"}
+                <Text style={[styles.content, styles.shadow]}>   Vale lembrar que pagamentos parcelados devem aparecer neste controle.
+                Assim fica ainda mais fácil ter uma boa projeção de fluxo de caixa.
+                </Text>
+                {"\n"}{"\n"}
+
+
+                <Text style={[styles.subtitle, styles.shadow]}>   Calcule o saldo operacional </Text>
+                {"\n"}{"\n"}
+                <Text style={[styles.content, styles.shadow]}>   Uma vez que as receitas e despesas são conhecidas, será possível
+                fazer o cálculo do saldo operacional da sua empresa.
+                {"\n"}{"\n"}
+                </Text>
+
+
+                <Text style={[styles.subtitle, styles.shadow]}>   Calcule o saldo período </Text>
+                {"\n"}{"\n"}
+                <Text style={[styles.content, styles.shadow]}>   E por fim, faça o cálculo do saldo final do período. Isso significa 
+                subtrair o valor do saldo operacional do saldo inicial.
+                {"\n"}{"\n"}
+                </Text>
+
+
+              </Text>
+            </Text>
           </View>
         </View>
 
@@ -36,6 +109,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#151515',
+  },
+  imagem: {
+    width: 358,
+    height: 240,
+    resizeMode: 'stretch',
+    marginHorizontal: 20
+
   },
   shadow: {
     shadowOffset: { width: 1, height: 1 },
