ec9073a6c6c09ebd47c6e79802385c123dfd40c1
Guilherme Serra <serra.guilherme@gmail.com>
2021-02-12 08:44:09 -0300

    Ajuste na identacao e no tamanho dos botoes indicadores.

--- src/components/ButtonsIndicators.js
+++ src/components/ButtonsIndicators.js
@@ -2,33 +2,36 @@ import React from "react";
 import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 export default function ButtonsIndicators(props) {
-    return (
-        <View style={styles.lineButtons}>
-            <TouchableOpacity style={ styles.button } onPress={ props.onPressLeft }>
-                <Image style={ styles.img }
-                source={ props.coverLeft }
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={ styles.button } onPress={ props.onPressRight }>
-                <Image style={ styles.img }
-                source={ props.coverRight }
-                />
-            </TouchableOpacity>
-          </View>
-    );
+  return (
+    <View style={styles.lineButtons}>
+      <TouchableOpacity style={styles.button} onPress={props.onPressLeft}>
+        <Image style={styles.img}
+          source={props.coverLeft}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={props.onPressRight}>
+        <Image style={styles.img}
+          source={props.coverRight}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-      margin: 3,
-      padding: 10,
-    },
-    img: {
-      width: 180,
-      height: 100,
-      borderRadius: 10
-    },
-    lineButtons: {
-        flexDirection: 'row'
-    }
-  });
+  button: {
+    margin: 3,
+    padding: 3,
+    width: '50%'
+  },
+  img: {
+    width: 180,
+    height: 100,
+    borderRadius: 10
+  },
+  lineButtons: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center'
+  }
+});
--- src/pages/Indicadores/index.js
+++ src/pages/Indicadores/index.js
@@ -4,45 +4,45 @@ import * as Animatable from 'react-native-animatable';
 
 import ButtonsIndicators from '../../components/ButtonsIndicators';
 
-export default function Indicadores( { navigation } ){
-    return(
-        <SafeAreaView style={{flex:1, backgroundColor:"#151515"}}>
+export default function Indicadores({ navigation }) {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#151515" }}>
             <Animatable.View useNativeDriver>
                 <ScrollView>
 
                     <ButtonsIndicators
-                    coverLeft={require('../../../assets/buttons/indicadores/patrimonio-liquido.png')}
-                    onPressLeft = { () => navigation.navigate('PLiquido') }
-                    coverRight={require('../../../assets/buttons/indicadores/divida-bruta.png')}
-                    onPressRight = { () => navigation.navigate('DividaBruta') }
+                        coverLeft={require('../../../assets/buttons/indicadores/patrimonio-liquido.png')}
+                        onPressLeft={() => navigation.navigate('PLiquido')}
+                        coverRight={require('../../../assets/buttons/indicadores/divida-bruta.png')}
+                        onPressRight={() => navigation.navigate('DividaBruta')}
                     />
 
                     <ButtonsIndicators
-                    coverLeft={require('../../../assets/buttons/indicadores/fluxo-caixa.png')}
-                    onPressLeft = { () => navigation.navigate('FluxoCaixa')}
-                    coverRight={require('../../../assets/buttons/indicadores/divida-liquida.png')}
-                    onPressRight = { () => navigation.navigate('DividaLiquida') }
+                        coverLeft={require('../../../assets/buttons/indicadores/fluxo-caixa.png')}
+                        onPressLeft={() => navigation.navigate('FluxoCaixa')}
+                        coverRight={require('../../../assets/buttons/indicadores/divida-liquida.png')}
+                        onPressRight={() => navigation.navigate('DividaLiquida')}
                     />
 
                     <ButtonsIndicators
-                    coverLeft={require('../../../assets/buttons/indicadores/roe.png')}
-                    onPressLeft = { () => navigation.navigate('Roe') }
-                    coverRight={require('../../../assets/buttons/indicadores/roa.png')}
-                    onPressRight = { () => navigation.navigate('Roa') }
+                        coverLeft={require('../../../assets/buttons/indicadores/roe.png')}
+                        onPressLeft={() => navigation.navigate('Roe')}
+                        coverRight={require('../../../assets/buttons/indicadores/roa.png')}
+                        onPressRight={() => navigation.navigate('Roa')}
                     />
 
                     <ButtonsIndicators
-                    coverLeft={require('../../../assets/buttons/indicadores/roic.png')}
-                    onPressLeft = { () => navigation.navigate('Roic') }
-                    coverRight={require('../../../assets/buttons/indicadores/ebit.png')}
-                    onPressRight = { () => {} }
+                        coverLeft={require('../../../assets/buttons/indicadores/roic.png')}
+                        onPressLeft={() => navigation.navigate('Roic')}
+                        coverRight={require('../../../assets/buttons/indicadores/ebit.png')}
+                        onPressRight={() => { }}
                     />
 
                     <ButtonsIndicators
-                    coverLeft={require('../../../assets/buttons/indicadores/ebitda.png')}
-                    onPressLeft = { () => {} }
-                    coverRight={require('../../../assets/buttons/indicadores/preco-vpa.png')}
-                    onPressRight = { () => {} }
+                        coverLeft={require('../../../assets/buttons/indicadores/ebitda.png')}
+                        onPressLeft={() => { }}
+                        coverRight={require('../../../assets/buttons/indicadores/preco-vpa.png')}
+                        onPressRight={() => { }}
                     />
 
                 </ScrollView>
