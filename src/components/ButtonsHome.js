8870bc5c56fdf7b150b3258492c5069e35c43fb1
Guilherme Serra <serra.guilherme@gmail.com>
2021-03-01 20:25:52 -0300

    Update ButtonsHome.js

--- src/components/ButtonsHome.js
+++ src/components/ButtonsHome.js
@@ -5,8 +5,9 @@ import { View } from "react-native-animatable";
 export default function ButtonsHome(props) {
   return (
     <View style={styles.view}>
-      <TouchableOpacity style={styles.button} onPress={props.onPress} />
-      <Text style={styles.text}>{props.text}</Text>
+      <TouchableOpacity style={styles.button} onPress={props.onPress}>
+        <Text style={styles.text}>{props.text}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -20,13 +21,14 @@ const styles = StyleSheet.create({
   button: {
     position: 'relative',
     width: '95%',
+    justifyContent: 'center',
     height: 80,
     borderRadius: 50,
     margin: 7,
     backgroundColor: '#d1286d'
   },
   text: {
-    position: 'absolute',
+    justifyContent: 'center',
     fontFamily: 'Righteous_400Regular',
     color: 'white',
     fontSize: 22,
