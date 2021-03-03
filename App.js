b8db99522e820d1898fa028cc10d12c2479b1ff0
Guilherme Serra <serra.guilherme@gmail.com>
2021-03-03 08:12:22 -0300

    Adicionado a escolha de plataforma.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import { Righteous_400Regular } from '@expo-google-fonts/righteous';
 import { AdMobBanner } from 'expo-ads-admob';
@@ -27,10 +27,10 @@ export default function App() {
       <StatusBar backgroundColor="#313e6a" barStyle="light-content" translucent={false} />
       <Routes />
 
-      <View style={{paddingBottom: 30}}>
+      <View style={{ paddingBottom: Platform.OS === 'ios' ? 30 : 0 }}>
         <AdMobBanner
           bannerSize="smartBannerPortrait"
-          adUnitID="ca-app-pub-3737795788074947/6585297873"
+          adUnitID={Platform.OS === 'ios' ? "ca-app-pub-3737795788074947/7584191374" : "ca-app-pub-3737795788074947/6585297873"}
           servePersonalizedAds
         />
       </View>
