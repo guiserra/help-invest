ef6c13e5606450506d841d4ee3bcbbfc59f89070
Guilherme Serra <serra.guilherme@gmail.com>
2021-03-03 19:17:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler'
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { StatusBar, Platform } from 'react-native';
+import { StatusBar, Platform, Dimensions } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import { Righteous_400Regular } from '@expo-google-fonts/righteous';
 import { AdMobBanner } from 'expo-ads-admob';
 
 import Routes from './src/router';
 import { View } from 'react-native-animatable';
+
+const deviceHeight = Dimensions.get("window").height
 
 export default function App() {
 
@@ -27,7 +29,7 @@ export default function App() {
       <StatusBar backgroundColor="#313e6a" barStyle="light-content" translucent={false} />
       <Routes />
 
-      <View style={{ paddingBottom: Platform.OS === 'ios' ? 30 : 1 }}>
+      <View style={{ paddingBottom: Platform.OS === 'ios' ? (deviceHeight * 0.029) : 1 }}>
         <AdMobBanner
           bannerSize="smartBannerPortrait"
           adUnitID={Platform.OS === 'ios' ? "ca-app-pub-3737795788074947/7584191374" : "ca-app-pub-3737795788074947/6585297873"}
