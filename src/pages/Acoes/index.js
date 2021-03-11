a6240ce883a1e623600817c7f4e6bff4f896342f
catakevin98 <catakevin98@github.com>
2021-03-11 18:14:57 -0300

    Otimização de código

--- src/pages/Acoes/index.js
+++ src/pages/Acoes/index.js
@@ -1,37 +1,50 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { ScrollView, SafeAreaView, View, Text, StyleSheet } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 import ButtonsInLine from '../../components/ButtonsInLine';
+import ButtonSmall from "../../components/ButtonSmall";
 
 export default function Acoes({ navigation }) {
+
+    const dados = [{textLeft: 'Definição', navigationLeft: 'DefinicaoAcao', textRight: 'Indicadores',  navigationRight: 'Indicadores'}, 
+                   {textLeft: 'Dividendos', navigationLeft: 'DefinicaoAcao', textRight: 'Tipos',  navigationRight: 'Indicadores'}];
+                   
+    function renderButtons (){
+        return(
+            dados.map((dado) => 
+                <ButtonsInLine                   
+                    textLeft= {dado.textLeft}
+                    onPressLeft={() => navigation.navigate(dado.navigationLeft)}
+
+                    textRight= {dado.textRight}
+                    onPressRight={() => navigation.navigate(dado.navigationRight)}
+                />
+            )
+        );
+    }
+    
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#313e6a" }}>
             <View/>
             <Animatable.View useNativeDriver>
-                <ScrollView>
-                    
+                <ScrollView showsVerticalScrollIndicator={false}>
                 <Text style={styles.text}>Ações</Text>
 
-                    <ButtonsInLine
-                        textLeft='Definição'
-                        onPressLeft={() => navigation.navigate('DefinicaoAcao')}
-                        textRight='Indicadores'
-                        onPressRight={() => navigation.navigate('Indicadores')}
-                    />
+                {renderButtons()}
 
-                    <ButtonsInLine
-                        textLeft='Patrimônio Líquido'
-                        onPressLeft={() => navigation.navigate('Indicadores')}
-                        textRight='Dívida Bruta'
-                        onPressRight={() => navigation.navigate('DividaBruta')}
-                    />
+                <ButtonSmall 
+                    text='Tributação'
+                    onPress={() => ('')}
+                />               
 
                 </ScrollView>
             </Animatable.View>
         </SafeAreaView>
     );
 }
+
 
 const styles = StyleSheet.create({
     text: {
